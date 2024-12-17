250a0801b041e775a5261de5f1693e27febefcc0
@hexfa <amir.dehdarian2009@gmail.com>
2024-12-17 21:16:57 +0330

    change token

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:todoist/presentation/views/project_page.dart';
 import 'core/di/di.dart';
 
 void main() {
-  const token = '0123456789abcdef0123456789';
+  const token = 'fd1df697c8622b190f2f2999047342d91e90690b';
 
   setupLocator(token);
   runApp(MyApp());
